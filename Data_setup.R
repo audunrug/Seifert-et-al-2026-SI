3884f4b29577cc9d35fad999a761fe52442f64cf
Audun Rugstad <77783308+audunrug@users.noreply.github.com>
2025-08-01 14:50:54 +0200

    cleanup the cleanup script

--- Data_setup.R
+++ Data_setup.R
@@ -8,10 +8,10 @@ TMB::openmp(parallel::detectCores()-1, autopar = TRUE, DLL = "gllvm")
 # load data
 full_data <- read.csv("data/full_dataset.csv", sep=";", dec = ",") 
 
+#### cleaning raw data #####
 # filter out duplicates
 full_data <- full_data |> filter(Species != "not done yet ",  # unfinished columns
                                  Classification != "L") # remove lichens
-
 full_data$SitePlot <- paste0(full_data$Name, full_data$Plot)
 
 # look for duplicates
@@ -21,7 +21,7 @@ as.data.frame(table(full_data$SitePlot, full_data$Species)) # 2 duplicates
 full_data <- full_data[-c(1401, 2003),]
 
 
-# make species dataframe 
+#### species occurrence data ####
 data_sp <- full_data |> # vascular + bryophytes
   select(Site, Plot, Species, Abundance) |> 
   pivot_wider(names_from = Species, 
@@ -40,7 +40,8 @@ data_sp_v <- full_data |> # only vascular
 data_sp_f <- data_sp[,colSums(data_sp > 0) > 6] # filter for species with only 5+ obs
 data_sp_v_f <- data_sp_v[,colSums(data_sp_v > 0) > 4] # filter for species with only 5+ obs
 
-## environmental dataframe
+
+#### environmental data ####
 data_env <- full_data |> 
   select(Name, Plot, distance_road, dist_t1, 
          Altitude_point, D.altitude, Operation_start,
@@ -48,29 +49,37 @@ data_env <- full_data |>
   distinct()
 data_env <- data_env[-90,]  # remove 1 typo row
 
+# road distance buffer zones
 data_env$dist_group <- ifelse(data_env$distance_road<=10,"<10", "10-50")
 data_env$dist_group[data_env$distance_road>50] <- ">50"
 
-# center and scale env data (NB: should I always do this?) 
-data_env$distance_road_raw <- data_env$distance_road # for plotting
-data_env$distance_road <- data_env$distance_road/10
-data_env$distance_road[data_env$dist_group=="10-50"] <- data_env$distance_road[data_env$dist_group=="10-50"] - 1
-data_env$distance_road[data_env$dist_group==">50"] <- data_env$distance_road[data_env$dist_group==">50"] - 5
+# scale/center road distance data
+data_env$distance_road_raw <- data_env$distance_road # for plotting?
+data_env$distance_road <- data_env$distance_road/10 # scale to every 10 meters
+data_env$distance_road[data_env$dist_group=="10-50"] <- # center 2nd buffer on 10 meters
+  data_env$distance_road[data_env$dist_group=="10-50"] - 1
+data_env$distance_road[data_env$dist_group==">50"] <- # center 3nd buffer on 50 meters
+  data_env$distance_road[data_env$dist_group==">50"] - 5
 
-
+# scale rest of environmental data
 data_env$dist_t1 <- scale(data_env$dist_t1)
 data_env$Altitude_point <- scale(data_env$Altitude_point)
 data_env$D.altitude <- scale(data_env$D.altitude)
 data_env$Bare_rock <- scale(data_env$Bare_rock)
+data_env$Bare_soil <- scale(data_env$Bare_soil)
 
 
-# traits
+#### trait data ####
 data_tr <- full_data |> 
   filter(Classification != "B", !(is.na(Rud)), !(is.na(L))) |> 
   select(Species, Rud, Com, Str, L, `F`, R, N, S) |> 
   distinct()
-data_env$Bare_soil <- scale(data_env$Bare_soil)
+
+# scale and center trait data
 data_tr <- data.frame(lapply(data_tr, function(x)if(is.numeric(x)){scale(x)}else{as.factor(x)}))
+
+# filter only species that have associated traits
 data_sp_4th <- data_sp[,data_tr$Species]
 
-save(data_sp, data_sp_f, data_sp_4th, data_env, data_tr, file="windpower_data.Rdata")
+# save data frames
+save(data_sp, data_sp_f, data_sp_4th, data_env, data_tr, file="data/windpower_data.Rdata")
